543a7913893d4ab6679ecdc73ae93fdb939d24a1
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-18 15:31:56 +0600

    updated

---
+++ app/src/main/java/com/example/creatingtableusingroomdatabase/UserDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.creatingtableusingroomdatabase
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+
+@Database(entities =[User::class], version = 1 )
+abstract class UserDatabase {
+
+abstract fun getUserDao() : UserDao
+
+
+companion object{
+    var db:UserDatabase?=null
+
+    public fun getInstance(context: Context):UserDatabase{
+
+if (db==null){
+    db= Room.databaseBuilder(
+        context,
+        UserDatabase::class.java,
+        "user_db"
+
+    ).allowMainThreadQueries().build()
+}
+
+
+
+        return db as UserDatabase
+    }
+}
+}
