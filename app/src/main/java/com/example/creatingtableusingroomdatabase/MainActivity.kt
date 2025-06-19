535352be80324b7cee1a0582b314d2b5973dd856
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-19 11:53:10 +0600

    updated

--- app/src/main/java/com/example/creatingtableusingroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/creatingtableusingroomdatabase/MainActivity.kt
@@ -1,29 +1,28 @@
+package com.example.creatingtableusingroomdatabase
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.creatingtableusingroomdatabase.User
-import com.example.creatingtableusingroomdatabase.UserDatabase
 import com.example.creatingtableusingroomdatabase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val userDao = UserDatabase.getInstance(this).getUserDao()
+        var user = User(0, "Rakib", 32, "01862240062")
+        var userDao = UserDatabase.getInstance(this).getUserDao()
 
         binding.insertbutn.setOnClickListener {
-            val name = binding.etName.text.toString()
-            val age = binding.etAge.text.toString().toIntOrNull() ?: 0
-            val mobile = binding.etMobile.text.toString()
+            userDao.userInsert(user)
 
-            if (name.isNotEmpty() && mobile.isNotEmpty()) {
-                val user = User(0, name, age, mobile)
-                userDao.userInsert(user)
-            }
         }
+
+
+
+
+
     }
-}
+} 
