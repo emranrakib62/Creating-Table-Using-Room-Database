34b4b9ed2442949fa265e55dc6e52ef0ddd34603
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-20 18:11:48 +0600

    updated

---
+++ app/src/main/java/com/example/creatingtableusingroomdatabase/UserAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.creatingtableusingroomdatabase
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.creatingtableusingroomdatabase.databinding.ItemUserBinding
+
+class UserAdapter :ListAdapter<User,UserAdapter.UserViewholder>(compator) {
+
+
+
+   class UserViewholder(var binding:ItemUserBinding):RecyclerView.ViewHolder(binding.root)
+
+companion object{
+
+
+    var compator= object : DiffUtil.ItemCallback<User>(){
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem==newItem
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.userid==newItem.userid
+        }
+
+
+    }
+}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewholder {
+     return  UserViewholder(ItemUserBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: UserViewholder, position: Int) {
+       getItem(position).let {
+           holder.binding.nametv.text=it.name
+           holder.binding.agetv.text="${it.age}"
+           holder.binding.mobiletv.text=it.mobile
+
+
+
+
+       }
+    }
+}
